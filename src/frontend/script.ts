9199240c62afe1dc9ee562c46c99531113d0de71
Joern Bernhardt <joern.bernhardt@compose.us>
2020-11-23 15:25:02 +0100

    Add randomized credit line
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/frontend/script.ts
+++ src/frontend/script.ts
@@ -5,6 +5,7 @@ const $y = document.getElementById("y") as HTMLInputElement;
 const $width = document.getElementById("width") as HTMLInputElement;
 const $height = document.getElementById("height") as HTMLInputElement;
 const $rotation = document.getElementById("rotation") as HTMLInputElement;
+const $credits = document.getElementById("credit-line");
 
 parent.postMessage(
   {
@@ -36,3 +37,32 @@ $form.onsubmit = (e: Event) => {
     "*"
   );
 };
+
+const creditTextPossibilities = [
+  ["Created by", "Developed by", "Produced by", "Written by"],
+  ["highly sophisticated", "some interesting", "the cool", "the insane"],
+  ["code monkeys", "folks", "people"],
+  [
+    "in various environments",
+    "in the mines of Moria",
+    "in snowy places",
+    "in the greatest nation of all... imagination",
+  ],
+];
+
+$credits.innerHTML = generateCreditLine();
+$credits.onmouseenter = () => {
+  $credits.innerHTML = generateCreditLine();
+};
+
+function randomOf(arr: string[]): string {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function generateCreditLine() {
+  const randomText = creditTextPossibilities.reduce(
+    (a, t) => [...a, randomOf(t)],
+    []
+  );
+  return `${randomText.join(" ")}.`;
+}
